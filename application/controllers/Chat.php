adbc8b493454e8ce92712d00150b597750ca6dd0
Nikita Stepanov <goowernor@gmail.com>
2017-01-28 22:35:41 +1000

    Add files via upload
    Добавлены страницы профиля и редактирования профиля

--- application/controllers/Chat.php
+++ application/controllers/Chat.php
@@ -1,5 +1,4 @@
 <?php
-
 class Chat extends CI_Controller{
     public function index(){
         $this->load->model('ChatModel');
@@ -27,54 +26,5 @@ class Chat extends CI_Controller{
             $this->load->view('templates/footer');
         }
     }
-    /*public function add_post(){
-        $this->load->model('Blog_model', 'blog');
-        
-        $data['title'] = 'Tutorial';
-        $data['heading'] = 'Добавление записи';
-        
-        if(isset($_POST['title']) and isset($_POST['post']))
-        {
-            $new_title = $_POST['title'];
-            $new_post = $_POST['post'];
-            $this->blog->Add_post($new_title, $new_post);
-        }
-        
-		$this->load->view('templates/header', $data);
-		$this->load->view('add_post', $data);
-        $this->load->view('templates/footer');
-    }*/
-    // За ненадобностью
-    /*public function all_posts()
-    {
-        $this->load->model('Blog_model', 'blog');
-        $this->load->helper('date');
-        
-        $data['title'] = 'Tutorial';
-        $data['heading'] = 'Просмотр всех записей';
-        $data['query'] = $this->blog->Get_all();
-        
-		$this->load->view('templates/header', $data);
-		$this->load->view('all_posts', $data);
-        $this->load->view('templates/footer');
-    }*/
-    /*public function post()
-    {
-        $this->load->model('Blog_model', 'blog');
-        $this->load->helper('date');
-        
-        $data['title'] = 'Tutorial';
-        $data['heading'] = 'Запись';
-        if(isset($_GET['id']))
-        {
-            $id = $_GET['id'];
-            $data['id'] = $this->blog->Post($id);
-        }
-        
-        
-		$this->load->view('templates/header', $data);
-		$this->load->view('post', $data);
-        $this->load->view('templates/footer');
-    }*/
 }
 ?>
